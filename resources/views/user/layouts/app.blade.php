58ce1b6378914d81e7fb45a3493d3ec9f017e172
Mark Lester Morta <marklester10.mlsm@gmail.com>
2020-06-06 13:53:19 +0800

    added favicon

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -15,3 +15,8 @@
 <!-- Custom styles for this template -->
 <link href="{{asset('user/css/clean-blog.min.css')}}" rel="stylesheet">
 <link href="{{ asset('user/css/prism.css') }}" rel="stylesheet" >
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
+<link rel="icon" type="image/png" sizes="512x512" href="{{ asset('/android-chrome-512x512.png') }}">
+<link rel="manifest" href="{{ asset('/site.webmanifest') }}">
--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -1,7 +1,9 @@
  <!-- Navigation -->
  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="{{route('resume')}}">Mark Lester</a>
+        <img class="navbar-brand" style="width: 90px;" src="{{ asset('/android-chrome-512x512.png') }}" alt="logo">
+      <a class="navbar-brand" href="/">
+        Mark Lester</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars"></i>
       </button>
